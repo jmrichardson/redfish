6448fa6f5af410ecb6aba256dd4978d0509b69a3
John Richardson <jmrichardson@gmail.com>
2019-10-30 15:59:23 -0400

    Create config.py

---
+++ rf/config.py
@@ -0,0 +1,17 @@
+# Set default variables
+iLO_https_url = "https://x.x.x.x"
+iLO_account = "admin"
+iLO_password = "admin"
+
+# Monitor
+monitor_log = 'logs/monitor.log'
+monitor_frequency = 5
+monitor = {
+  '/redfish/v1/Systems/*Members*': [
+    'Oem/Hpe/SystemUsage/CPUUtil',
+    'Oem/Hpe/SystemUsage/MemoryBusUtil'
+    ],
+  '/redfish/v1/Chassis/*Members*': [
+    'Status/Health'
+  ]
+}
