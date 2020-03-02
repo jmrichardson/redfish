b78b3fa3f9e49313191e305306a1a893d1fddbf0
John Richardson <jmrichardson@gmail.com>
2020-03-02 14:29:17 -0500

    update2

--- firmware.py
+++ firmware.py
@@ -1,9 +1,14 @@
 from rf.rf import update, rf
 
-# Set variables
+# Upload to iLO repository
 update_repo = True
+
+# Update firmware
 update_target = False
+
+# Firmware file location
 firmware = 'firmware/ilo/2.14/ilo5_214.bin'
+
 # firmware = 'firmware/ilo/1.4/ilo5_140.bin'
 
 # Update firmware
